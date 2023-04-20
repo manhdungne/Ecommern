30c70fdff375f18a19c8427d607ecaded6a4e51e
manhdungne <99822739+manhdungne@users.noreply.github.com>
2023-04-21 00:06:08 +0700

    update

--- client/src/services/appApi.js
+++ client/src/services/appApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const appApi = createApi({
   reducerPath: "appApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8080" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://nhom13.onrender.com" }),
   endpoints: (builder) => ({
     signup: builder.mutation({
       query: (user) => ({
